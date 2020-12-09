4be468ca011327585ec4691dacd82933c4707049
Riyad0821 <mdriyad0821@gmail.com>
2020-12-09 19:32:47 +0600

    design, server activation and message send funtionality done

--- src/Firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCnvpt0ae_6stRhWi77Llr986WLDJnpkLg",
-    authDomain: "whatsapp-mern-ce076.firebaseapp.com",
-    databaseURL: "https://whatsapp-mern-ce076.firebaseio.com",
-    projectId: "whatsapp-mern-ce076",
-    storageBucket: "whatsapp-mern-ce076.appspot.com",
-    messagingSenderId: "430397057830",
-    appId: "1:430397057830:web:22b39d63c805f3e01cefe7"
-  };
